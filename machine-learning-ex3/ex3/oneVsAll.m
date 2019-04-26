f0d77eec331dbdcbad77f22d752427344c8a7f45
Valley <valley.zw@gmail.com>
2019-04-26 09:14:54 +0800

    Add solutions

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -57,7 +57,7 @@ options = optimset('GradObj', 'on', 'MaxIter', 50);
 % train each classifier
 for i = 1:num_labels
     all_theta(i,:) = fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), initial_theta, options);
-endfor
+end
 
 
 
